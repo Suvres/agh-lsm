d6e5d67e74ea3604c82ba6e99578f63eda546e55
Suvres <bartosz.blyszcz@gmail.com>
2020-12-29 18:42:36 +0100

    base html twig

--- src/Controller/DefaultController.php
+++ src/Controller/DefaultController.php
@@ -1,24 +1,19 @@
 <?php
 
 
-namespace Controller;
+namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
-/**
- * Class DefaultController
- */
 class DefaultController extends AbstractController
 {
-
     /**
      * @Route("/", name="home")
      */
     public function indexAction(): Response
     {
-
         return $this->render("default/index.html.twig");
     }
 }

85cfaa0da420a30e4629a5746a820602567f5aad
Suvres <bartosz.blyszcz@gmail.com>
2021-01-13 01:16:37 +0100

    ec

--- test_db.sh
+++ test_db.sh
@@ -4,5 +4,3 @@ bin/console --env=test doctrine:data:drop --force
 bin/console --env=test doctrine:data:create --if-not-exists
 bin/console --env=test doctrine:schema:update --force
 bin/console --env=test doctrine:fi:lo -v -n
-
-
